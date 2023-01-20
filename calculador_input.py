8e3bcb6d8a689bf21e62b3a217e1eecd6a8df755
Yuri Silva <121751241+yuribertozzi@users.noreply.github.com>
2023-01-20 09:00:03 -0300

    Add files via upload

---
+++ calculador_input.py
@@ -0,0 +1,15 @@
+a = input("Digite o valor de a: ")
+b = input("Digite o valor de b: ")
+c = input("Digite o valor de c: ")
+d = input("Digite o valor de d: ")
+e = input("Digite o valor de e: ")
+
+a = float(a)
+b = float(b)
+c = float(c)
+d = float(d)
+e = float(e)
+
+conta=a+(b+c)**2-d*e
+
+print ("O resultado de a + (b + c) ** 2 - d * e é", conta)
